56d268bbaf65362aa19003caea5c41684d508932
Aidan Okes <AOkes@users.noreply.github.com>
2020-03-31 19:15:04 -0600

    HW-8
    Late upload, or I thought reupload

---
+++ HW-8/readme.md
@@ -0,0 +1,9 @@
+# 441 HW-8
+For this assignment, it took me some time to get used to using the jQuery. Although
+I understood how it worked, it took some time for me to get it properly running, as
+I was having some difficulty getting the code to work the way I intended it to work.
+Once I did however, I felt really pleased with what I could do with it. Things that would
+have normally taken me many hours to get working took a much shorter amount of time when
+I was using jQuery. I decided to use the opportunity to make an homage to my dog,
+Templeton, who passed away last summer, and I felt I was able to do him justice using the
+tools provided by jQuery
---
+++ HW-8/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+dogs();
+
+setInterval(function(){dogs()}, 4000);
+
+//Has dogs appear in a set order onscreen
+function dogs(){
+    $('#dog1').delay(800).fadeOut("slow").fadeIn("slow");
+    $('#dog2').delay(1000).fadeOut("slow").fadeIn("slow");
+    $('#dog3').delay(1200).fadeOut("slow").fadeIn("slow");
+    $('#dog4').delay(1400).fadeOut("slow").fadeIn("slow");
+    $('#dog5').delay(1600).fadeOut("slow").fadeIn("slow");
+    $('#dog6').delay(1800).fadeOut("slow").fadeIn("slow");
+    $('#dog7').delay(2000).fadeOut("slow").fadeIn("slow");
+    $('#dog8').delay(2200).fadeOut("slow").fadeIn("slow");
+    $('#dog9').delay(2400).fadeOut("slow").fadeIn("slow");
+    $('#dog10').delay(2600).fadeOut("slow").fadeIn("slow");
+}
+
+//The button starts the slideshow
+      $('#btn_play').click(function(){
+        $('.dog').hide( function(){
+          $('#btn_play').hide( function(){
+
+            $('#title').text("An Omage to my old pet, Templeton...").delay(2000).fadeOut(function(){
+
+            beginSlideshow();
+
+        });
+      });
+    });
+  });
+      var text = new Array("A sweetheart", "A stinker", "Templeton --- RIP");
+
+      var image_src = new Array("./images/sweetheart.jpg", "./images/stinker.jpg")
+
+  function beginSlideshow(){
+
+                $('#descriptionText').html(text[0]).animate({fontSize: "2em"}, "slow").fadeOut(2000, function(){
+                    $('#descriptionText2').html(text[1]).animate({fontSize: "2em"}, "slow").fadeOut(4000, function(){
+                      $('#ending').html(text[2]).animate({fontSize: "3em"}, "slow");
+                    });
+                });
+                $('#mainImage').attr('src', image_src[0]).fadeOut(2000, function(){
+                  $('#mainImage2').delay(2000).attr('src', image_src[1]).fadeOut(4000);
+                });
+
+    }
+
+});
